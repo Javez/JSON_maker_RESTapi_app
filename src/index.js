eaf6f797779a169b8a37122e3aa7902dd4de6d45
Алексей Данилов <mr.javez@ukr.net>
2024-03-28 23:45:38 +0200

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import express from "express";
+import express from 'express';
+import bodyParser from 'body-parser';
+import router from './routes/json.routes.js';
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(router);
+
 app.listen(3000, () => {
-    console.log("Server started on port 3000");
+    console.log('Server started on port 3000');
 });
